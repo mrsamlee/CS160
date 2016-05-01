776298073a1881145dc89ae77f14605302f0f428
Samson Lee <samson.lee@sjsu.edu>
2016-05-01 15:45:32 -0700

    fixed migration issue and authentication before action

--- lazysandwich/app/controllers/postings_controller.rb
+++ lazysandwich/app/controllers/postings_controller.rb
@@ -1,4 +1,5 @@
 class PostingsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_posting, only: [:show, :edit, :update, :destroy]
 
   # GET /postings
--- lazysandwich/db/migrate/20160429044647_add_posting_to_sandwich.rb
+++
@@ -1,5 +0,0 @@
-class AddPostingToSandwich < ActiveRecord::Migration
-  def change
-  	add_column :sandwiches, :posting_id, :integer
-  end
-end
