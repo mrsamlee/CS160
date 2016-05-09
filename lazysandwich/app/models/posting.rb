5499b2f251ab5569956fe1c17dd25194ada69e5f
WNG <wilson_ng1ve@yahoo.com>
2016-05-09 16:26:10 -0700

    updated webpage

--- lazysandwich/app/models/posting.rb
+++ lazysandwich/app/models/posting.rb
@@ -2,11 +2,13 @@ class Posting < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
   # has_many :owner, foreign_key: "posting_id", class_name: "Transaction"
-
+  
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.street_changed? and obj.city_changed? and obj.state_changed?}
 
   def address
     [street, city, state, country].compact.join(', ')
   end
+  
+  validates_presence_of :street, :city, :name, :ingredients
 end
