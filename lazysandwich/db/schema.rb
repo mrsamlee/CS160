63b364e281ae390f8e237dba7d1ef4550126e0a8
WNG <wilson_ng1ve@yahoo.com>
2016-05-09 17:50:08 -0700

    Added About page

--- lazysandwich/db/schema.rb
+++ lazysandwich/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507112611) do
+ActiveRecord::Schema.define(version: 20160510000205) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "postings", force: :cascade do |t|
     t.datetime "created_at",  null: false
